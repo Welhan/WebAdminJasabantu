9c10c4ee17777d799a0bd4f5afde458e11e4893b
welhan <78246470+Welhan@users.noreply.github.com>
2024-03-27 11:38:24 +0700

    pembaruan database seader

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\UserModel;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $faker = Faker::create();
+        for ($i = 0; $i < 10; $i++) {
+            UserModel::create([
+
+                'UniqueID' => $faker->randomDigit,
+                'Name' => $faker->name,
+                'Email' => $faker->unique()->safeEmail,
+                'Pin' => bcrypt($faker->numberBetween(100000, 999999)), // Password yang sama untuk semua pengguna, ganti sesuai kebutuhan Anda
+                'Phone' => $faker->phoneNumber(), // Password yang sama untuk semua pengguna, ganti sesuai kebutuhan Anda
+            ]);
+        }
     }
 }
